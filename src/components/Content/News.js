05cdce448404954bc2c6770b6a692bb2248b2cdf
Julio Memeli <memeli>
2021-06-21 17:03:11 -0300

    add news and events

--- src/components/Content/News.js
+++ src/components/Content/News.js
@@ -3,7 +3,8 @@ import React from 'react'
 const NewsHome = () => {
     return (
         <>
-            <div className="h-56 bg-white relative flex flex-col items-center justify-center mb-12 overflow-hidden">
+        <div className="relative flex flex-row items-center justify-center p-8">
+            <div className="h-56 bg-white relative flex flex-col items-center space-between justify-center mb-12 overflow-hidden mr-28">
                 <div className="h-auto font-light flex flex-col items-center p-2 m-auto">
                     <h1 className="text-3xl">Notícias/Atualizações</h1>
                 </div>
@@ -24,12 +25,12 @@ const NewsHome = () => {
                 </div>
                 <div className="h-auto relative flex flex-col items-center justify-center p-2 overflow-hidden">
                     <div className="h-auto relative flex flex-row items-center justify-center p-2">
-                        <div className="items-center text-white bg-blue-500 p-2 flex justify-center font-light mr-2 rounded-md">
+                        <div className="items-center text-white bg-red-500 p-2 flex justify-center font-light mr-2 rounded-md">
                             05/04
                         </div>
                         <div className="h-auto relative flex flex-col p-2">
                             <div>
-                                Lançamento do servidor
+                                Manutenção do servidor
                             </div>
                             <div>
                                 <small>O lançamento do servidor foi adiado para... <a className="hover:text-blue-500 hover:underline" href="/" alt="Ver Mais">Ver Mais</a></small>
@@ -39,12 +40,12 @@ const NewsHome = () => {
                 </div>
                 <div className="h-auto relative flex flex-col items-center justify-center p-2 overflow-hidden">
                     <div className="h-auto relative flex flex-row items-center justify-center p-2">
-                        <div className="items-center text-white bg-blue-500 p-2 flex justify-center font-light mr-2 rounded-md">
+                        <div className="items-center text-white bg-yellow-500 p-2 flex justify-center font-light mr-2 rounded-md">
                             05/04
                         </div>
                         <div className="h-auto relative flex flex-col p-2">
                             <div>
-                                Lançamento do servidor
+                                Atualização do servidor
                             </div>
                             <div>
                                 <small>O lançamento do servidor foi adiado para... <a className="hover:text-blue-500 hover:underline" href="/" alt="Ver Mais">Ver Mais</a></small>
@@ -52,6 +53,58 @@ const NewsHome = () => {
                         </div>
                     </div>
                 </div>
+            </div>
+
+            <div className="h-56 bg-white relative flex flex-col items-center justify-center mb-12 overflow-hidden">
+                <div className="h-auto font-light flex flex-col items-center p-2 m-auto">
+                    <h1 className="text-3xl">Eventos</h1>
+                </div>
+                <div className="h-auto relative flex flex-col items-center justify-center p-2 overflow-hidden">
+                    <div className="h-auto relative flex flex-row items-center justify-center p-2">
+                        <div className="items-center text-white bg-yellow-900 p-2 flex justify-center font-light mr-2 rounded-md">
+                            05/04
+                        </div>
+                        <div className="h-auto relative flex flex-col p-2">
+                            <div>
+                                Evento finalizado
+                            </div>
+                            <div>
+                                <small>O lançamento do servidor foi adiado para... <a className="hover:text-blue-500 hover:underline" href="/" alt="Ver Mais">Ver Mais</a></small>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div className="h-auto relative flex flex-col items-center justify-center p-2 overflow-hidden">
+                    <div className="h-auto relative flex flex-row items-center justify-center p-2">
+                        <div className="items-center text-white bg-green-500 p-2 flex justify-center font-light mr-2 rounded-md">
+                            05/04
+                        </div>
+                        <div className="h-auto relative flex flex-col p-2">
+                            <div>
+                                Evento Acontecendo
+                            </div>
+                            <div>
+                                <small>O lançamento do servidor foi adiado para... <a className="hover:text-blue-500 hover:underline" href="/" alt="Ver Mais">Ver Mais</a></small>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div className="h-auto relative flex flex-col items-center justify-center p-2 overflow-hidden">
+                    <div className="h-auto relative flex flex-row items-center justify-center p-2">
+                        <div className="items-center text-white bg-indigo-500 p-2 flex justify-center font-light mr-2 rounded-md">
+                            05/04
+                        </div>
+                        <div className="h-auto relative flex flex-col p-2">
+                            <div>
+                                Proximo evento
+                            </div>
+                            <div>
+                                <small>O lançamento do servidor foi adiado para... <a className="hover:text-blue-500 hover:underline" href="/" alt="Ver Mais">Ver Mais</a></small>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
             </div>
         </>
     )
