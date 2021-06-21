964fd90b7bb1d1139c0ce4acd8fbac38f2a96a58
Julio Memeli <memeli>
2021-06-21 05:16:20 -0300

    fix navbar

--- src/components/Header/Navbar.js
+++ src/components/Header/Navbar.js
@@ -1,7 +1,7 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
-import { FaDiscord, FaCannabis } from 'react-icons/fa'
-import { AiFillFacebook } from 'react-icons/ai'
+import { FaCannabis } from 'react-icons/fa'
+
 const Navbar = () => {
     return (
         <nav className="z-30 flex justify-between items-center h-16 bg-white text-black relative shadow-md" role="navigation">
